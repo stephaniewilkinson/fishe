624892f17b77dd9c13fc7b2bad7d928e564f7137
Stephanie Wilkinson <what.happens@gmail.com>
2016-06-01 12:25:39 -0700

    create scientist table

---
+++ db/migrate/20160601192517_create_scientists.rb
@@ -0,0 +1,11 @@
+class CreateScientists < ActiveRecord::Migration
+  def change
+    create_table :scientists do |t|
+      t.string :name
+      t.string :discipline
+      t.boolean :spooky
+
+      t.timestamps null: false
+    end
+  end
+end
